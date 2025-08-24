f08da17ff76bffe59a57a22e7938c455801babfb
Nikita Tsvetkov <tsv1@fastmail.com>
2025-08-24 21:41:19 +0400

    Add test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,17 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+    test('Extension should be present', () => {
+        const extension = vscode.extensions.getExtension('vedro.vedro');
+        assert.ok(extension, 'Extension should be available');
+    });
+
+    test('Extension should activate', async () => {
+        const extension = vscode.extensions.getExtension('vedro.vedro');
+        await extension?.activate();
+        assert.ok(extension?.isActive);
+    });
+
 });
